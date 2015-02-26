26cb608031e2dd3ffde5a62213797333898a1217
Romain LEGUAY <romain.leguay@gmail.com>
2015-02-26 23:21:14 +0100

    Fix license to remove it from doxygen

--- lib/policy/ResultPolicy.hxx
+++ lib/policy/ResultPolicy.hxx
@@ -1,4 +1,4 @@
-/**
+/*
  * The MIT License (MIT)
  *
  * Copyright (c) <2015> <Romain LEGUAY romain.leguay@gmail.com>
@@ -20,7 +20,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
-**/
+ */
+
 /*
  * ResultPolicy.hxx
  *
