f74045e4464d0d6ee57d1210a14eb63dd91519a3
Romain LEGUAY <romain.leguay@gmail.com>
2015-02-26 20:39:33 +0100

    Add UNUSED macro

---
+++ lib/util/Macro.hpp
@@ -0,0 +1,16 @@
+/*
+ * Macro.hpp
+ *
+ *  Created on: 26 févr. 2015
+ *      Author: rleguay
+ */
+
+#ifndef MACRO_HPP_
+#define MACRO_HPP_
+
+/**
+ * This macro avoid warning of unused parameter
+ */
+#define UNUSED(x) (void)(x)
+
+#endif /* MACRO_HPP_ */
